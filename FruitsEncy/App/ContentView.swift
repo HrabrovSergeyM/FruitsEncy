d43f8fa6cee9cb8b1f58d70bf28424d9ef6cc6bf
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-07-13 23:35:05 +0300

    main: Implemented Fruit List

--- FruitsEncy/App/ContentView.swift
+++ FruitsEncy/App/ContentView.swift
@@ -8,19 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: - Properties
+    
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: - Body
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationView {
+            List {
+                ForEach(fruits) { fruit in
+                    FruitRowView(fruit: fruit)
+                        .listRowSeparator(.hidden)
+                }
+            } // List
+            .listStyle(PlainListStyle())
+            .navigationTitle("Fruits")
+        } // NavigationView
     }
 }
 
+// MARK: - Preview
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(fruits: fruitsData)
     }
 }
