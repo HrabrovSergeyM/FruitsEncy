86a077ce30231f12d716cad9a1440b30f27b4be0
HrabrovSergeyM <69889536+HrabrovSergeyM@users.noreply.github.com>
2023-07-13 11:25:45 +0300

    Initial Commit

---
+++ FruitsEncy/FruitsEncyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsEncyApp.swift
+//  FruitsEncy
+//
+//  Created by Sergey Hrabrov on 13.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FruitsEncyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
